fc4af14d1b837ca69253cf1b0a6df08945fce735
Trevor Storey <trevorstorey23@gmail.com>
2017-11-08 19:35:41 -0500

    [tutorial], learning react native

--- App.js
+++ App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
 
-export default class App extends React.Component {
+export default class JustifyContentBasics extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>heyoooooo</Text>
+      // Try setting `justifyContent` to `center`.
+      // Try setting `flexDirection` to `row`.
+      <View style={{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'stretch'
+      }}>
+        <View style={{ height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{ height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{ height: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Open up App.js to start working on your app!</Text>
+//         <Text>Changes you make will automatically reload.</Text>
+//         <Text>Shake your phone to open the developer menu.</Text>
+//         <Text>heyoooooo</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
